d92d5dfe98309eff1aed6326402136527921ecfd
Byohazyrd <benjaminmikkelson@gmail.com>
2021-06-05 22:59:45 -0600

    added navbar, generated user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
+  devise_for :users, :controlllers => { registrations: 'registrations' }
   resources :events
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
