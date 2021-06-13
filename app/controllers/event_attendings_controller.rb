f3e834ce4a649c4718710a01368605923ba583ea
Byohazyrd <benjaminmikkelson@gmail.com>
2021-06-12 21:19:56 -0600

    prevented duplicate registrations

--- app/controllers/event_attendings_controller.rb
+++ app/controllers/event_attendings_controller.rb
@@ -12,15 +12,24 @@ class EventAttendingsController < ApplicationController
   
   def create
     @event = set_event()
-    attendance = @event.event_attendings.build(event_attending_params)
+    if @event.host.id == current_user.id
+      flash[:alert] = "You're the host! You don't need to register for your own event."
+      redirect_to @event
+    elsif @event.guests.include?(current_user)
+      flash[:alert] = "You're already registered for this event."
+      redirect_to @event
+    else
 
-    respond_to do |format|
-      if attendance.save
-        format.html { redirect_to @event, notice: "You have successfully registered for the event." }
-        format.json { render :show, status: :created, location: @event_attending }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @event_attending.errors, status: :unprocessable_entity }
+      attendance = @event.event_attendings.build(event_attending_params)
+
+      respond_to do |format|
+        if attendance.save
+          format.html { redirect_to @event, notice: "You have successfully registered for the event." }
+          format.json { render :show, status: :created, location: @event_attending }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @event_attending.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
